04931e5e75dd90c3299b9d74a50a6a1a92d78a8b
Гребенюк Костянтин Олегович <Konstantin.O.Grebenyuk@alfabank.kiev.ua>
2021-11-21 13:54:19 +0200

    19c

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ubuntu-itea" {
 
   vpc_security_group_ids = [aws_security_group.itea-all.id]
 
-  key_name = "mac_central.pem"
+  key_name = "mac_central"
 
   tags = {
     "Name" = "ubuntu-itea"
