bc7dc56ff36c7656e40623ba1f6ba95da5dc8885
Гребенюк Костянтин Олегович <Konstantin.O.Grebenyuk@alfabank.kiev.ua>
2021-11-21 12:05:57 +0200

    5 commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "ubuntu-itea" {
   ami           = "ami-0bd9c26722573e69b"
   instance_type = "t3.micro"
 
-  subnet_id = aws_subnet.lesson12-subnet-public-3.id
+  subnet_id = aws_subnet.itea-subnet-public-3.id
 
-  vpc_security_group_ids = [aws_security_group.lesson12-all.id]
+  vpc_security_group_ids = [aws_security_group.itea-all.id]
 
   key_name = "mac-koc"
 
